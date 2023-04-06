89843aac134e706f82ffd8cee6b189850de4fdb0
Raony Reis <75029692+raonygamer@users.noreply.github.com>
2023-04-06 18:15:11 -0300

    Implement new mod callback api

--- src/Example.cpp
+++ src/Example.cpp
@@ -2,14 +2,24 @@
 
 #include "generated/initcpp.h"
 
-class Example : public Zenova::Mod {
-	virtual void Start() {}
-	virtual void Update() {}
-	virtual void Tick() {}
-	virtual void Stop() {}
-	virtual ~Example() {}
-};
+// Called when the Mod is loaded
+MOD_FUNCTION void ModLoad(ModContext& ctx) {
+	Zenova_Info("Mod {} loaded!", ctx.folder);
+}
 
-MOD_FUNCTION Zenova::Mod* CreateMod() {
-	return new Example;
+// Called after Minecraft main
+// todo: implement above (currently called after ~2 seconds)
+MOD_FUNCTION void ModStart() {
+	Zenova_Info("Mod Started!");
+}
+
+// Called every Minecraft tick (must be in world)
+// todo: implement above (currently calls every 1/20th second)
+MOD_FUNCTION void ModTick() {
+
+}
+
+// Called on clean up/exit
+MOD_FUNCTION void ModStop() {
+	Zenova_Info("Mod Stopped!");
 }
